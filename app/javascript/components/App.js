29b0dfc9e64bf4f645c71d3be1b0f2a97644808d
migueldp4 <migueldp4@gmail.com>
2021-03-24 12:36:54 -0600

    add index component to views

---
+++ app/javascript/components/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>This is my new app!</div>
+        <div>This is my new app!</div>
+        <div>This is my new app!</div>
+        <div>This is my new app!</div>
+        <div>This is my new app!</div>
+      </div>
+    )
+  }
+}
+
+export default App
