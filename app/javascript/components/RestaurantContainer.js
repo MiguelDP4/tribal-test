08b4a4cc5558101107035f5467462b22fd9b2ec0
migueldp4 <migueldp4@gmail.com>
2021-03-24 22:59:47 -0600

    Add react router to manage pages

--- app/javascript/components/RestaurantContainer.js
+++ app/javascript/components/RestaurantContainer.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Restaurant from "./RestaurantCard";
+
 const RestaurantContainer = props => {
   const {restaurants} = props
   return (
--- app/javascript/components/RestaurantDetail.js
+++ app/javascript/components/RestaurantDetail.js
@@ -8,11 +8,11 @@ const RestaurantDetail = props => {
   const image = images.find(i => i.nameId === restaurant.nameId);
   return (
     <div className='restaurant-detail-container'>
-      <header className='restaurant-detail-header'>
+      <div className='restaurant-detail-header'>
         <img src={image.content} alt='restaurant-logo'/>
         <h2>{restaurant.name}</h2>
         <p>{restaurant.description}</p>
-      </header>
+      </div>
     </div>
   );
 }
