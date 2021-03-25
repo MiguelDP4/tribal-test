a612ef6e5429174cfccdb52b85fcec9f02f238d1
migueldp4 <migueldp4@gmail.com>
2021-03-24 18:24:38 -0600

    import demo image

--- app/javascript/components/RestaurantCard.js
+++ app/javascript/components/RestaurantCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import logo1 from "images/logos/bread-grind-logo.png";
 
 const RestaurantCard = props => {
   const {restaurant} = props;
   return (
     <div className="restaurant">
-      <img src={'images/bread-grind-logo.png'} alt={`${restaurant.nameId}-logo`} />
+      <img src={logo1} alt={`${restaurant.nameId}-logo`} />
       <h4>{restaurant.name}</h4>
       <p>{restaurant.description}</p>
       <a href="#">VER MAS</a>
