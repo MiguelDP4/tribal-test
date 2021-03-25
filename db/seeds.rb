320a0aa52459be1966191fd255d31553bb63dbae
migueldp4 <migueldp4@gmail.com>
2021-03-25 12:03:10 -0600

    Seed deletes content on database before seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
- 
+Comment.delete_all
+Restaurant.delete_all
+
 Restaurant.create!(nameId: 'bread-grind',name: 'Bread Grind', description: 'Pan hecho con granos molidos en el restaurante')
 Restaurant.create!(nameId: 'ham-and-eggs',name: 'Ham and Eggs', description: 'Los mejores desayunos que probará')
 Restaurant.create!(nameId: 'italian',name: 'Sun & Moon Italian Dining', description: 'Restaurante italiano con los mejores vinos de la region')
