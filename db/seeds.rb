40375b66f4fabbcfedf3870c1e68f7f72b15ab8c
migueldp4 <migueldp4@gmail.com>
2021-03-24 21:09:57 -0600

    add restaurant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ 
+Restaurant.create!(nameId: 'bread-grind',name: 'Bread Grind', description: 'Pan hecho con granos molidos en el restaurante')
+Restaurant.create!(nameId: 'ham-and-eggs',name: 'Ham and Eggs', description: 'Los mejores desayunos que probará')
+Restaurant.create!(nameId: 'italian',name: 'Sun & Moon Italian Dining', description: 'Restaurante italiano con los mejores vinos de la region')
+Restaurant.create!(nameId: 'japanese-food',name: 'Japanese Food', description: 'Comida japonesa hecha por cocineros japoneses')
+Restaurant.create!(nameId: 'mxdowells',name: "McDowell's", description: 'Mejores hamburguesas que en comida rápida')
+Restaurant.create!(nameId: 'pops',name: "Pop's Diner", description: 'Comida las 24 horas')
